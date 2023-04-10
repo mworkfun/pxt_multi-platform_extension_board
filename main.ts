ee7f644a9e86150f26c3274212d69ffb1f4ac1c1
mworkfun <39076163+mworkfun@users.noreply.github.com>
2023-04-10 19:57:34 +0800

    init

--- main.ts
+++ main.ts
@@ -11,7 +11,8 @@ const enum segment{
     d  = 0b00001000,
     c  = 0b00000100,
     b  = 0b00000010,
-    a  = 0b00000001
+    a  = 0b00000001,
+    ' ' = 0b00000000
 }
 
 const enum OnOff {
@@ -36,8 +37,58 @@ const enum Veer {
 }
 
 //% color="#ff6800" icon="\uf002" weight=15
-//% groups="['Display-Buttom', 'Led', 'RGB-Led', 'Humiture', 'Ultrasonic', 'Fan', 'Buzzer', 'Button']"
+//% groups="['Display-Buttom', 'Led', 'RGB-Led', 'Humiture', 'Ultrasonic', 'Fan', 'Buzzer', 'Button', 'Storer']"
 namespace Multi_platform {
+    // Compute a Dallas Semiconductor 8 bit CRC directly.
+    // this is much slower, but a little smaller, than the lookup table.
+    // https://www.analog.com/en/technical-articles/understanding-and-using-cyclic-redundancy-checks-with-maxim-1wire-and-ibutton-products.html
+    function crc8(addr: any, len: number) {
+        let i: number = 0;
+        let a: number = 0;
+        let crc: number = 0;
+        while (len--) {
+            crc ^= addr[a];
+            a++;
+            for (i = 0; i < 8; ++i) {
+                // Anti-order CRC8
+                // 1. X8+X5+X4+1 = 100110001 		  
+                // 2. The calculation of reverse XOR is used : 100110001 ---> 100011001
+                // 3. The lowest bit of data is not processed : 100011001 ---> 10001100
+                //    (Move (discard) one bit if the lowest bit of both the data and the polynomial is 1)
+                // 4. 10001100 = 0x8C
+                if (crc & 0x01)
+                    crc = (crc >> 1) ^ 0x8C;
+                else
+                    crc = (crc >> 1);
+            }
+        }
+        return crc;
+    }
+
+    function crc16(input: any, len: number) {
+        let i: number = 0;
+        let a: number = 0;
+        let crc: number = 0x0000;
+        while (len--) {
+            crc ^= input[a];
+            a++;
+            for (i = 0; i < 8; ++i) {
+                // Anti-order CRC16
+                // 1. X16+X15+X2+1 = 11000000000000101 		  
+                // 2. The calculation of reverse XOR is used : 11000000000000101 ---> 10100000000000011
+                // 3. The lowest bit of data is not processed : 10100000000000011 ---> 1010000000000001
+                //    (Move (discard) one bit if the lowest bit of both the data and the polynomial is 1)
+                // 4. 1010000000000001 = 0xA001
+                if (crc & 0x01)
+                    crc = (crc >> 1) ^ 0xA001;
+                else
+                    crc = (crc >> 1);
+            }
+        }
+        return crc;
+    }
+
+
     // They correspond to 4-bit digital tube and can control 8 digital sections of the code tube.
     // default = 0xff, bit: on = 0, off = 1
     // D7  D6  D5  D4  D3  D2  D1  D0
@@ -199,7 +250,7 @@ namespace Multi_platform {
     // Number : 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15
     // Position : 0, 1, 2, 3
     //% block="Digital-Tube_Num: position $Position num $Number"
-    //% Position.min=0 Position.max=3 Number.min=0 Number.max=15
+    //% Position.min=0 Position.max=3 Number.min=0 Number.max=9
     //% group="Digital-Tube_Button" weight=3
     export function Digital_Tube_Num(Position: number, Number: number) {
         if (Position > 3 || Number > 15)
@@ -273,7 +324,7 @@ namespace Multi_platform {
     ////////////////////////////////////////////
     // LED
     //% block="Set $Pos led  $OnOff"
-    //% Pos.min=0 Pos.max=7
+    //% Pos.min=0 Pos.max=7 OnOff.min=0 OnOff.max=1
     //% group="Led" weight=1
     export function Set_Led(Pos: number, OnOff: number){
         if (Pos > 8 || OnOff > 1) {
@@ -347,7 +398,7 @@ namespace Multi_platform {
     }
 
     ////////////////////////////////////////////
-    //% block="AHT20_Init"
+    //% block="Humiture_init"
     //% group="Humiture" weight=3
     ////////////////////////////////////////////
     export function AHT20_Init(){
@@ -436,6 +487,7 @@ namespace Multi_platform {
 
     ////////////////////////////////////////////
     //% block="Fan $_Veer speed $Speed"
+    //% Speed.min=0 Speed.max=1023
     //% group="Fan" weight=1
     ////////////////////////////////////////////
     export function Fan_(_Veer: Veer, Speed: number) {
@@ -463,5 +515,224 @@ namespace Multi_platform {
     //% group="Button" weight=1
     export function Button_pin() {
         return 5;
+    }
+
+
+    // Perform the onewire reset function.  We will wait up to 250uS for
+    // the bus to come high, if it doesn't then it is broken or shorted
+    // and we return a 0;
+    // Returns 1 if a device asserted a presence pulse, 0 otherwise.
+    function OneWire_reset(){
+        let r: number;
+        let retries: number = 125;
+        // wait until the wire is high... just in case
+        do {
+            if (--retries == 0) return 0;
+            control.waitMicros(2);
+        } while (!pins.digitalReadPin(DigitalPin.P7));
+
+        pins.digitalWritePin(DigitalPin.P7, 0);
+        control.waitMicros(550);
+        r = pins.digitalReadPin(DigitalPin.P7);         
+        control.waitMicros(410);
+        return r;
+    }
+
+    // Write a bit. Port and bit is used to cut lookup time and provide
+    // more certain timing.
+    function OneWire_write_bit(v: number){
+        if (v & 1) {   // write bit 1	
+            pins.digitalWritePin(DigitalPin.P7, 0);
+            control.waitMicros(10);
+            pins.digitalWritePin(DigitalPin.P7, 1);
+            control.waitMicros(55);
+        } else {      // write bit 0
+            pins.digitalWritePin(DigitalPin.P7, 0);
+            control.waitMicros(65);
+            pins.digitalWritePin(DigitalPin.P7, 1);
+            control.waitMicros(5);
+        }
+    }
+
+    // Read a bit. Port and bit is used to cut lookup time and provide
+    // more certain timing.
+    function OneWire_read_bit(){
+        let r: number;
+        pins.digitalWritePin(DigitalPin.P7, 0);
+        control.waitMicros(13);
+        r = pins.digitalReadPin(DigitalPin.P7);
+        control.waitMicros(53);
+        return r;
+    }
+
+    // Write a byte. 
+    function OneWire_write_byte(v: number) {
+        let bitMask: number;
+        for (bitMask = 0x01; bitMask; bitMask <<= 1) {
+            OneWire_write_bit((bitMask & v) ? 1 : 0);
+        }
+    }
+
+    // Read a byte
+    function OneWire_read_byte() {
+        let bitMask: number;
+        let r: number = 0;
+        for (bitMask = 0x01; bitMask; bitMask <<= 1) {
+            if (OneWire_read_bit()) r |= bitMask;
+        }
+        return r;
+    }
+
+
+
+    let ROM_NUM = [0,0,0,0,0,0,0,0];
+
+    ////////////////////////////////////////////
+    //% block="Search_device"
+    //% group="Storer" weight=6
+    export function EEPROM_search_ROM(){
+        let id_bit: number = 0;
+        let cmp_id_bit: number = 0;
+        let b: number = 0;
+        let i: number = 0;
+
+        for (i = 0; i < 8; i++)
+            ROM_NUM[i] = 0;
+
+        // The device will be reset and found.
+        // If the device is not found, return false.
+        if (!OneWire_reset())
+            return false;
+
+        OneWire_write_byte(0xF0);     // Search ROM
+
+        do {
+            // read a bit and its complement
+            id_bit = OneWire_read_bit();
+            cmp_id_bit = OneWire_read_bit();
+            // check for no devices on 1-wire
+            if ((id_bit == 1) && (cmp_id_bit == 1)) {
+                return false;
+            } else {
+                if ((id_bit == 0) && (cmp_id_bit == 0))
+                    // Multiple devices found on 1-wire
+                    return false;
+            }
+            if (id_bit)
+                ROM_NUM[b / 8] |= 0x01 << (b % 8);
+
+            OneWire_write_bit(id_bit);
+            b++;
+        } while (b < 64);
+
+        // A device was found but the serial number CRC is invalid.
+        if (crc8(ROM_NUM, 7) != ROM_NUM[7])
+            return false;
+
+        // Family code error
+        if (ROM_NUM[0] != 0x2D)
+            return false;
+
+        return true;
+    }
+
+    function EEPROM_check_crc16(input: any, len: number, inverted_crc: any) {
+        let crc: number = ~crc16(input, len);
+        return (crc & 0xFF) == inverted_crc[0] && (crc >> 8) == inverted_crc[1];
+    }
+
+    // slect ROM
+    function EEPROM_slect_rom(){
+        let i: number = 0;
+        OneWire_reset();              // initial signal
+        OneWire_write_byte(0x55);     // Match ROM
+        for (i = 0; i < 8; i++)
+        OneWire_write_byte(ROM_NUM[i]);
+    }
+
+    ////////////////////////////////////////////
+    //% block="Get_device"
+    //% group="Storer" weight=5
+    export function Return_ROM() {
+        return ROM_NUM;
+    }
+
+    // Read a byte of data from eeprom.
+    //% block="Read data from $address"
+    //% address.min=0 address.max=16
+    //% group="Storer" weight=4
+    export function EEPROM_read(address: number){
+        address = address*8;
+        EEPROM_slect_rom();              // Match ROM
+        OneWire_write_byte(0xF0);     // read memory
+        OneWire_write_byte(address&0x00ff);
+        OneWire_write_byte((address&0xff00)>>8);
+        return OneWire_read_byte();
+    }
+
+    // Write a 8-byte row, must write 8 bytes at a time.
+    //% block="Write $buf to $address"
+    //% address.min=0 address.max=16
+    //% group="Storer" weight=3
+    export function EEPROM_write(address: number, buf: any){
+        let verify: boolean = false;
+        let crc16 = [0,0];    // store value of crc
+        let buffer = [0,0,0,0,0,0,0,0,0,0,0,0];       // data)+command = 12bytes
+        let i: number = 0;
+
+        // 1.write scratchpad --> Write data to the scratchpad
+        buffer[0] = 0x0F;                   // store commands --> write scratchpad
+        buffer[1] = address & 0x00ff;       // address
+        buffer[2] = (address & 0xff00) >> 8;
+        for(i=0; i<8; i++){
+            buffer[i + 3] = buf[i];         // 8 bytes data
+        }   
+
+        EEPROM_slect_rom();                        // Match ROM
+        OneWire_write_byte(buffer[0]);          // CMD ---> write scratchpad
+        OneWire_write_byte(buffer[1]);          // address
+        OneWire_write_byte(buffer[2]);
+
+        for (i = 3; i < 11; i++)  // write 8 bytes data to eeprom
+        OneWire_write_byte(buffer[i]);
+
+        crc16[0] = OneWire_read_byte();         // Read CRC-16
+        crc16[1] = OneWire_read_byte();
+        if (!EEPROM_check_crc16(buffer, 11, crc16))
+            verify = true; //CRC not matching, try to read again
+
+        // 2.read scratchpad --> Read data from the scratchpad
+        buffer[0] = 0xAA;                   // store commands --> read scratchpad
+        EEPROM_slect_rom();                        // Match ROM
+        OneWire_write_byte(buffer[0]);          // CMD ---> read scratchpad
+
+        for (i = 1; i < 4; i++)            //Read TA1(Low address), TA2(High address) and E/S
+            buffer[i] = OneWire_read_byte();
+
+        if (buffer[3] != 0x07)              // E/S must be equal to 0x07(8 bytes data)
+            return false;
+
+        if (verify) {
+            for (i = 4; i < 12 ; i++) //Read the data of scratchpad(8 bytes)
+                buffer[i] = OneWire_read_byte();
+
+            crc16[0] = OneWire_read_byte();        // Read CRC-16
+            crc16[1] = OneWire_read_byte();
+            if (!EEPROM_check_crc16(buffer, 12, crc16))  // CRC not matching.
+                return false;
+        }
+
+        // 3.Copy scratchpad --> Write the data in the scratchpad to memory
+        buffer[0] = 0x55;          // CMD --> Copy scratchpad
+        EEPROM_slect_rom();               // Match ROM
+        for (i = 0; i < 4; i++)   //Send authorization code (TA1, TA2, E/S)
+        OneWire_write_byte(buffer[i]);
+
+        basic.pause(15);                 // t_PROG = 12.5ms worst case.
+        let res: number = OneWire_read_byte();  // Read copy status, 0xAA = success
+        if (res != 0xAA) {
+            return false;
+        }
+        return true;
     }
 }
